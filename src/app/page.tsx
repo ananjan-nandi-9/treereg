62b8e92471bd76d036e1d51bbf1942f5d4182125
MurtyShikhar <shikhar.murty@gmail.com>
2025-02-04 15:52:53 -0800

    OG image + preview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import Head from "next/head";
 
 const sections = [
     { id: "abstract", title: "Abstract" },
